ab48b207622f5f71bfb46f9d9f0a325d80dd8482
yrong <yrong1997@gmail.com>
2021-12-01 00:51:57 +0800

    release binary only

--- .maintain/publish-release.sh
+++ .maintain/publish-release.sh
@@ -14,17 +14,3 @@ make build-bifrost-release
 cp target/release/bifrost ${WORK_PATH}/resources
 chmod +x ${WORK_PATH}/resources/bifrost
 tar cjSf ${WORK_PATH}/resources/bifrost-x86_64-linux-gnu.tar.bz2 ${WORK_PATH}/resources/bifrost
-
-
-###### build wasm
-docker run --rm -it \
-  -e PACKAGE=$RUNTIME-runtime \
-  -e VERBOSE=1 \
-  -e CARGO_TERM_COLOR=always \
-  -v ${TMPDIR}/cargo:/cargo-home \
-  -v ${WORK_PATH}:/build \
-  paritytech/srtool:${RUSTC_VERSION} build ${EXTRA_ARGS}
-
-cp ${WORK_PATH}/runtime/$RUNTIME/target/srtool/release/wbuild/$RUNTIME-runtime/${RUNTIME}_runtime.compact.compressed.wasm \
-${WORK_PATH}/resources
-tar cjSf ${WORK_PATH}/resources/bifrost-wasm.tar.bz2 ${WORK_PATH}/resources/${RUNTIME}_runtime.compact.compressed.wasm
